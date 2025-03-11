3d00c34e59cac9c51a9809f3107f03697201304f
jewell-owen <jewell.owen@yahoo.com>
2025-03-11 10:38:51 -0500

    fixed image size and contrast for MIPS

--- portfolio/src/components/Projects/Projects.jsx
+++ portfolio/src/components/Projects/Projects.jsx
@@ -7,6 +7,7 @@ import { ProjectCard } from "./ProjectCard";
 
 
 export const Projects = () => {
+    
     return (
         <section className={styles.container} id="projects">
             <h2 className={styles.title}>Projects</h2>
