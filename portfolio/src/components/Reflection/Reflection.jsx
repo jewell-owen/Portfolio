acf629fa9d64ebb9516dc70289e44b74185092c9
jewell-owen <jewell.owen@yahoo.com>
2025-11-16 20:34:18 -0600

    added a placeholder for senior design

--- portfolio/src/components/Reflection/Reflection.jsx
+++ portfolio/src/components/Reflection/Reflection.jsx
@@ -2,8 +2,17 @@ import styles from "./Reflection.module.css";
 
 export const Reflection = () => (
   <section className={styles.container} id="reflection">
-    <h2 className={styles.title}>Reflections</h2>
+    <h2 className={styles.title}>Resume & Reflections</h2>
     <ul className={styles.list}>
+            <li>
+        <a
+          href={`${import.meta.env.BASE_URL}assets/reflection/owen_jewell_resume.pdf`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Resume (PDF)
+        </a>
+      </li>
       <li>
         <a
           href={`${import.meta.env.BASE_URL}assets/reflection/reflection1.pdf`}
